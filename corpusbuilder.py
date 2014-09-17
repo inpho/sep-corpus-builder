7b19bfd48671994bbb0831fffe76e81a16c5f6d7
rzawar <zawar.rohit@gmail.com>
2014-09-16 23:52:08 -0400

    added the extract_articlee_body function

--- corpusbuilder.py
+++ corpusbuilder.py
@@ -1,9 +1,42 @@
 import os
 import os.path
 import sys
-
-import inpho.corpus.sep as sep
 from codecs import open
+
+
+def extract_article_body(filename):
+    """
+    Extracts the article body from the SEP article at the given filename. Some
+    error handling is done to guarantee that this function returns at least the
+    empty string. Check the error log.
+    """
+    f = open(filename)
+    doc = f.read()
+    soup = BeautifulSoup(doc, convertEntities=["xml", "html"])
+
+    # rip out bibliography
+    biblio_root = soup.findAll('h2', text='Bibliography')
+    if biblio_root:
+        biblio_root = biblio_root[-1].findParent('h2')
+        if biblio_root:
+            biblio = [biblio_root]
+            biblio.extend(biblio_root.findNextSiblings())
+            biblio = [elm.extract() for elm in biblio]
+        else:
+            logging.error('Could not extract bibliography from %s' % filename)
+
+    # grab modified body 
+    body = soup.find("div", id="aueditable")
+    if body is not None:
+        # remove HTML escaped characters
+        body = re.sub("&\w+;", "", body.text)
+    
+        return body
+    else:
+        logging.error('Could not extract text from %s' % filename)
+
+        return '':
+
 
 def build_corpus(entriesfile, output_dir):
     # check if output_dir exists, if not make it
