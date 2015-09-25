6834d78b4e64f06fb74a7aee69aadc1cbce18e37
Jessie Pusateri <jessiepusateri@gmail.com>
2015-09-25 17:38:51 -0400

    migrating print statements to logging

--- corpusbuilder.py
+++ corpusbuilder.py
@@ -19,14 +19,14 @@ def getStyleBibliography(biblioList):
                 parsed = anystyle.parse((h.unescape(biblio).encode('utf-8')))
                 anyStyleList.append(parsed)
         return anyStyleList	
+
 def process_archives():
     for root,dirs,files in os.walk("/var/inphosemantics/sep-archives/db/"):
         for sem_year in dirs:
             year = sem_year[-4:]
             if int(year) > 2006:
                 path = root+sem_year+"/entries.txt"
-                print path
-                print "PROCESSING", sem_year
+                logging.info("Current path: %s", path) 
                 build_corpus(path ,"data/" + sem_year + "/", sem_year);
 
 def extract_article_body(filename):
@@ -73,27 +73,29 @@ def extract_bibliography(filename):
                         for li in ul.findAll('li'):
                                 bib.append(li.text)
         else:
-            print "No bibliography found"
+            logging.warning("No bibliography found")
 
         return bib
+
 def build_corpus(entriesfile, output_dir,sem_year):
     # check if output_dir exists, if not make it
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    count = 0
+    # keep of tally of the successful reads
+    success_read = 0
     with open(entriesfile, 'rb') as csvfile:
         for row in csvfile:
             sep_dir = row.split("::")[0]
             filename="/var/inphosemantics/sep-archives/raw/"+sem_year+"/entries/"+sep_dir+"/index.html"
             if not os.path.exists(filename):
-                print "NO FILE FOR", sep_dir
+                logging.warning('No file for %s', sep_dir)
                 continue
             else:
-                count = count + 1
-                print "SUCCESS " + str(count)
+                success_read = success_read + 1
             plain_filename = os.path.join(output_dir, '%s.txt' % sep_dir)
             with open(plain_filename, 'wb', 'utf-8') as plainfile:
                 plainfile.write(extract_article_body(filename))
+        logging.info("Successful reads: %d", success_read)
 
 if __name__ == '__main__':
     """
@@ -107,4 +109,5 @@ if __name__ == '__main__':
 
     build_corpus(args.entries, args.output)
     """
+    logging.basicConfig(format='%(levelname)s:%(message)s', filename='sep_corpus.log', level = logging.INFO)
     process_archives()
