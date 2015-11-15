bc0ce81786e2f79d979e27289004ffd8c848442e
Jessie Pusateri <jessiepusateri@gmail.com>
2015-11-15 15:11:31 -0500

    added archive_sort_function to sort the versions by date

--- recent_archives.py
+++ recent_archives.py
@@ -1,4 +1,5 @@
 import os
+import operator
 from glob import glob
 from collections import defaultdict
 
@@ -6,9 +7,19 @@ def generate_archive_dict():
      entries = defaultdict(list)
      files = [os.path.basename(x) for x in glob('/home/jessiepusateri/sep-corpus-builder/data/*.txt')]
      for filename in files:
-         season, article = filename.replace('.txt', ',').split('-', 1)
+         season, article = filename.replace('.txt', '').split('-', 1)
          entries[article].append(season)
-
+     return entries
+    
+def archive_sort_function(date):
+    season_order = ["spr", "sum", "fall", "win"]
+    return date[-4:], season_order.index(date[:-4])
+    
+def sort_seasons(season_list):
+    return sorted(season_list, key = archive_sort_function)
 
 if __name__ == '__main__':
-    generate_archive_dict()
+    versions = generate_archive_dict()
+    for entry in versions:
+        print sort_seasons(versions[entry])
+
